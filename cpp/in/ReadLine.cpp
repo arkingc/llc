f616a45f069e52985cd5ae3a654323cf130b9a07
cxm_mac <243744327@qq.com>
2018-01-15 14:19:19 +0800

    modify

--- cpp/in/ReadLine.cpp
+++ cpp/in/ReadLine.cpp
@@ -8,7 +8,8 @@ int main()
 	string line;
 	/*
 	 * 行读取
-	 *		如果希望整行整行保存输入
+	 *	如果希望整行整行保存输入
+	 *	换行符也会被读入cin，但是不会保存到line中
 	 */
 	while(getline(cin,line)){
 		cout<<line<<endl;
--- cpp/in/ReadWord.cpp
+++ cpp/in/ReadWord.cpp
@@ -8,8 +8,8 @@ int main()
 	string word;
 	/*
 	 * 单词读取
-	 *		每回车一次将一行返回给cin
-	 *		空格被作为分隔符
+	 *	每回车一次将一行返回给cin
+	 *	空格被作为分隔符
 	 */
 	while(cin >> word){
 		cout<<word<<endl;
