997bb9b9cca8db02c5797e591b02126bfa82924a
chenximing <243744327@qq.com>
2018-04-20 09:17:33 +0800

    修改

--- cpp/sizeof.cpp
+++ cpp/sizeof.cpp
@@ -15,18 +15,17 @@ void Test(int Array[],int Size)
 int main(){
 
     char *string1 = "tencent2018";
-    int size1 = sizeof(string1);
-    int size2 = sizeof(*string1);
+    int size1 = sizeof(string1);            //8
+    int size2 = sizeof(*string1);           //1
     char string2[100] = "tencent2018";
-    int size3 = sizeof(string2);            //并不是把string2当成一个指针处理
-
-    //8 1 100
-    cout << size1 << " " << size2 << " " << size3 << endl;
+    int size3 = sizeof(string2);            //100，并不是把string2当成一个指针处理
+    char string3[] = "hello";               
+    int size4 = sizeof(string3);            //6，保存结尾\0
 
     int arr1[] = {1,2,3,4};
     int arr2[10] = {1};
-    cout << sizeof(arr1) << endl;           //16
-    cout << sizeof(arr2) << endl;           //40
+    cout << sizeof(arr1) << endl;           //16，没有当作指针
+    cout << sizeof(arr2) << endl;           //40，没有当作指针
 
     Test(arr1, sizeof(arr1)/sizeof(int));
 
